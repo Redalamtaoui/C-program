89d0525f60acd63faaeb1a15687ab802913800dc
Redalamtaoui <84287455+Redalamtaoui@users.noreply.github.com>
2021-06-12 03:46:22 +0100

    Create ft_strcnpy.c

---
+++ ft_strcnpy.c
@@ -0,0 +1,9 @@
+char *ft_strncpy(char *dest , char *src , unsigned int n){
+         dest = src;
+        unsigned int i = 0;
+        while (i < n ){
+        ft_putchar(src[i]);
+        i++;
+        }
+return 0;
+}
