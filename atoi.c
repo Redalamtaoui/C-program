ed025661882108454d05578c4b6be610e5857f11
Redalamtaoui <84287455+Redalamtaoui@users.noreply.github.com>
2021-06-12 01:23:50 +0100

    Update atoi.c

--- atoi.c
+++ atoi.c
@@ -1,36 +1,29 @@
 int ft_atoi(char *str){
-        int i = 0;
-        int h = 0;
-        int res = 0;
-        int f = 1;
-        int s = 1;
-while(f != 0){
-        if (str[i] >= 'a' && str[i] <= 'z' ){
+        int i = 0 ;
+        int nbr = 1;
+        int result = 0;
+while(str){
+        if (str[i] >= 'a' || str[i] >= 'A' ){
         ft_putnbr(0);
-        }
-      if (str[0] == '-' && str[1] >= 'a'){
- ft_putnbr(0);
-break;
-        }
+        break;}
+      if (str[0] == '-' && str[1] >= 'a' ||  str[1] >= 'A' ){
+ft_putnbr(0);
+break;}
       if (str[i] == '-'){
  ft_putchar('-');
-i++;
-        }
-
+i++;}
+    if (str[0] == '0'){
+        i = 1;
+                }
+    if (str[0] == '-' && str[1] == '0'){
+            i = 2;
+    }
 while(str[i] != '\0'){
-if (str[i] >= 'a' && str[i] <= 'z' ){
-break;
-}
-else{
-ft_putnbr( res * 10 + str[i] - '0');
-i++;
-}
-}
-f = 0;
-}
-ft_putchar('\n');
-return 0;
-}
-int main(){
-ft_atoi("5676G8");
-}
+if (str[i] >= 'a' || str[i] >= 'A'){
+break;}
+        else {
+ft_putnbr( 0 + str[i] - '0');
+i++;}}
+break;}
+return 0;}
+
